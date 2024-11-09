bd816f04a864aa953c4aa2e4019ca481f7ab2027
Ivan Nichipor <https://gitlab.gitlab.bcs.ru/NichiporIV>
2024-11-09 09:24:27 +0300

    start

---
+++ BankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Ivan Nichipor on 9.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
